f5718f3f19b9750edb50ac577f89ba9e0a6ca98a
ss <ss@sonya>
2018-10-20 12:52:06 +0300

    Set up Spring, Java 9, MySQL.

---
+++ src/main/java/org/ss/mvd/entity/Document.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 ss.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.ss.mvd.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Document.
+ * @author ss
+ */
+@Entity
+@Table(name = "document")
+public class Document implements Serializable {
+    /** Default UID. */
+    private static final long serialVersionUID = 1L;
+// ================================ FIELDS ====================================
+    /** ID. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    /** User-friendly identifier. */
+    @NotNull
+    @Size(max = 10)
+    @Column(name = "identifier", length = 10)
+    private String identifier;
+    /** Document type. */
+    @JoinColumn(name = "document_type_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private DocumentType type;
+// ================================ SET & GET =================================
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    /**
+     * @return the identifier
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+    /**
+     * @param identifier the identifier to set
+     */
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+    /**
+     * @return the type
+     */
+    public DocumentType getType() {
+        return type;
+    }
+    /**
+     * @param type the type to set
+     */
+    public void setType(DocumentType type) {
+        this.type = type;
+    }
+// ============================================================================
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getId() != null ? getId().hashCode() : 0);
+        return hash;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Document)) {
+            return false;
+        }
+        Document other = (Document) object;
+        return !((this.id == null && other.id != null)
+                || (this.id != null && !this.id.equals(other.id)));
+    }
+    @Override
+    public String toString() {
+        return "org.ss.mvd.entity.Document[ id=" + getId() + " ]";
+    }
+}
